9c8d92e402b8cfaaaac8eceefc801eab2c49917a
cjskldjfkjs <zhelyazko.iv@gmail.com>
2024-05-15 19:55:41 +0200

    Main menu done

--- Assets/Code/Tablet_Input.cs
+++ Assets/Code/Tablet_Input.cs
@@ -4,27 +4,46 @@ using UnityEngine;
 
 public class Tablet_Input : MonoBehaviour
 {
-    private RaycastHit hit;
     public Camera mainCamera;
-    public GameObject HitPoint;
+    public GameObject HitPoint, Tablet;
+    public Animator animator;
+    public GameObject Player;
+
+    private Movement_for_planer movement_For_Planer;
+    private RaycastHit hit;
     // Start is called before the first frame update
     void Start()
     {
-        
+        movement_For_Planer = Player.GetComponent<Movement_for_planer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
-            { 
-                hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 20, ForceMode.Impulse);
-                hit.collider.gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(1,10),
-                    Random.Range(10, 50), Random.Range(10, 20)), ForceMode.Impulse);
-            }
-        }  
+        //if(Input.GetMouseButtonDown(0))
+        //{
+        //    Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        //    if (Physics.Raycast(ray, out hit)/* && hit.collider.gameObject.CompareTag("startButton")*/)
+        //    { 
+        //        hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 20, ForceMode.Impulse);
+        //        hit.collider.gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(1,10),
+        //            Random.Range(10, 50), Random.Range(10, 20)), ForceMode.Impulse);
+        //    }
+        //}  
+    }
+    private void OnMouseDown()
+    {
+        gameObject.transform.SetParent(Tablet.transform);
+        animator.SetBool("IsButtonPressed", true);
+        Tablet.GetComponent<Rigidbody>().AddForce(-transform.forward * 20, ForceMode.Impulse);
+        Tablet.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(1, 10),
+        Random.Range(10, 50), Random.Range(10, 20)), ForceMode.Impulse);
+        movement_For_Planer.BeginTheJournej();
+        StartCoroutine(Selfdestraction());
+    }
+    private IEnumerator Selfdestraction()
+    {
+        yield return new WaitForSeconds(10f);
+        Destroy(Tablet);
     }
 }
