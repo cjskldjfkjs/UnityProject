328ad8cfd0b2bfc429eff137d411f3c18c0e6da1
cjskldjfkjs <zhelyazko.iv@gmail.com>
2024-09-25 18:06:09 +0200

    Still dealing with a new problem of distance

--- Assets/Code/Distance_traveld_indicator.cs
+++ Assets/Code/Distance_traveld_indicator.cs
@@ -7,20 +7,40 @@ public class Distance_traveld_indicator : MonoBehaviour
 {
     public GameObject startPoint;
 
-    private float distance;
+    private float distance, i;
+
+    private bool switchIndicators;
 
     public Image distanceIndicator, distanceIndicator1;
     // Start is called before the first frame update
     void Start()
     {
-        distanceIndicator.GetComponent<RectTransform>().sizeDelta = new Vector2(0,0);
+        distanceIndicator.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 0);
         distanceIndicator1.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 0);
+        switchIndicators = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        distance = Mathf.Abs((gameObject.transform.position.z - startPoint.transform.position.z)/5); 
-        distanceIndicator.GetComponent<RectTransform>().sizeDelta = new Vector2(distance, distance);
+        if (!switchIndicators && i % 2 == 0)
+        {
+            distance = Mathf.RoundToInt(Mathf.Abs((gameObject.transform.position.z - startPoint.transform.position.z) / 5));
+            distanceIndicator.GetComponent<RectTransform>().sizeDelta = new Vector2(distance, distance);
+        }
+
+        if (distance >= 250)
+        {
+            switchIndicators = true;
+            distance = 0;
+            i++;
+        }
+
+        if (switchIndicators && i % 2 != 0)
+        {
+            startPoint.transform.position = new Vector3(0, 0, startPoint.transform.position.z + 103 * i);
+            distance = Mathf.Abs((gameObject.transform.position.z - startPoint.transform.position.z) / 5);
+            distanceIndicator1.GetComponent<RectTransform>().sizeDelta = new Vector2(distance, distance);
+        }
     }
 }
