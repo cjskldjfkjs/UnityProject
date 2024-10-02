10bd6822a9c5d643db051dcecd7724dc0cb67a61
cjskldjfkjs <zhelyazko.iv@gmail.com>
2024-10-02 18:00:34 +0200

    80% of distance metter ready

--- Assets/Code/Distance_traveld_indicator.cs
+++ Assets/Code/Distance_traveld_indicator.cs
@@ -7,40 +7,54 @@ public class Distance_traveld_indicator : MonoBehaviour
 {
     public GameObject startPoint;
 
-    private float distance, i;
+    private float distance1, distance2, i, DCanvas1, DCanvas2;
 
     private bool switchIndicators;
 
     public Image distanceIndicator, distanceIndicator1;
+
+    [SerializeField]
+    private GameObject DistanceCanvas1, DistanceCanvas2;
     // Start is called before the first frame update
     void Start()
     {
         distanceIndicator.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 0);
         distanceIndicator1.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 0);
         switchIndicators = false;
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (!switchIndicators && i % 2 == 0)
-        {
-            distance = Mathf.RoundToInt(Mathf.Abs((gameObject.transform.position.z - startPoint.transform.position.z) / 5));
-            distanceIndicator.GetComponent<RectTransform>().sizeDelta = new Vector2(distance, distance);
+        if (distance1<250 && !switchIndicators)
+        {   
+            distance1 = Mathf.RoundToInt(Mathf.Abs((gameObject.transform.position.z - startPoint.transform.position.z) / 5));
+            distanceIndicator.GetComponent<RectTransform>().sizeDelta = new Vector2(distance1, distance1);
         }
 
-        if (distance >= 250)
+        else if (distance1 >= 250 && !switchIndicators)
         {
             switchIndicators = true;
-            distance = 0;
-            i++;
+            startPoint.transform.position = new Vector3(0, 0, startPoint.transform.position.z + 1000);
+            distance2 = 0;
+            DistanceCanvas1.GetComponent<Canvas>().sortingOrder = 0;
+            DistanceCanvas2.GetComponent<Canvas>().sortingOrder = 1;
         }
 
-        if (switchIndicators && i % 2 != 0)
+        if (distance2 < 250 && switchIndicators)
+        {           
+            distance2 = Mathf.RoundToInt(Mathf.Abs((gameObject.transform.position.z - startPoint.transform.position.z) / 5));
+            distanceIndicator1.GetComponent<RectTransform>().sizeDelta = new Vector2(distance2, distance2);
+        }
+
+        else if (distance2 >= 250 && switchIndicators)
         {
-            startPoint.transform.position = new Vector3(0, 0, startPoint.transform.position.z + 103 * i);
-            distance = Mathf.Abs((gameObject.transform.position.z - startPoint.transform.position.z) / 5);
-            distanceIndicator1.GetComponent<RectTransform>().sizeDelta = new Vector2(distance, distance);
+            switchIndicators = false;
+            startPoint.transform.position = new Vector3(0, 0, Mathf.Abs(startPoint.transform.position.z + 1000));
+            distance1 = 0;
+            DistanceCanvas1.GetComponent<Canvas>().sortingOrder = 1;
+            DistanceCanvas2.GetComponent<Canvas>().sortingOrder = 0;
         }
     }
 }
